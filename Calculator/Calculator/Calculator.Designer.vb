01ce336e2f2d2dbcc83b3a3d4c842a7ef69e21d3
RichardIli <ili.richard.recana@gmail.com>
2023-07-12 02:50:13 -0700

    First Calculator Commit

---
+++ Calculator/Calculator/Calculator.Designer.vb
@@ -0,0 +1,329 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalculatorForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.subtractButton = New System.Windows.Forms.Button()
+        Me.multiplyButton = New System.Windows.Forms.Button()
+        Me.divideButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.equalButton = New System.Windows.Forms.Button()
+        Me.addButton = New System.Windows.Forms.Button()
+        Me.dotButton = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.enternewButton = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(344, 100)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(0, 66)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(344, 33)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "=0"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label2
+        '
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(0, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(344, 33)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "  0"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(344, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "  0"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.enternewButton)
+        Me.Panel3.Controls.Add(Me.subtractButton)
+        Me.Panel3.Controls.Add(Me.multiplyButton)
+        Me.Panel3.Controls.Add(Me.divideButton)
+        Me.Panel3.Controls.Add(Me.clearButton)
+        Me.Panel3.Controls.Add(Me.equalButton)
+        Me.Panel3.Controls.Add(Me.addButton)
+        Me.Panel3.Controls.Add(Me.dotButton)
+        Me.Panel3.Controls.Add(Me.Button0)
+        Me.Panel3.Controls.Add(Me.Button3)
+        Me.Panel3.Controls.Add(Me.Button2)
+        Me.Panel3.Controls.Add(Me.Button1)
+        Me.Panel3.Controls.Add(Me.Button6)
+        Me.Panel3.Controls.Add(Me.Button5)
+        Me.Panel3.Controls.Add(Me.Button4)
+        Me.Panel3.Controls.Add(Me.Button9)
+        Me.Panel3.Controls.Add(Me.Button8)
+        Me.Panel3.Controls.Add(Me.Button7)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 100)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(344, 489)
+        Me.Panel3.TabIndex = 2
+        '
+        'subtractButton
+        '
+        Me.subtractButton.Location = New System.Drawing.Point(255, 6)
+        Me.subtractButton.Name = "subtractButton"
+        Me.subtractButton.Size = New System.Drawing.Size(75, 75)
+        Me.subtractButton.TabIndex = 17
+        Me.subtractButton.Text = "-"
+        Me.subtractButton.UseVisualStyleBackColor = True
+        '
+        'multiplyButton
+        '
+        Me.multiplyButton.Location = New System.Drawing.Point(174, 6)
+        Me.multiplyButton.Name = "multiplyButton"
+        Me.multiplyButton.Size = New System.Drawing.Size(75, 75)
+        Me.multiplyButton.TabIndex = 16
+        Me.multiplyButton.Text = "*"
+        Me.multiplyButton.UseVisualStyleBackColor = True
+        '
+        'divideButton
+        '
+        Me.divideButton.Location = New System.Drawing.Point(93, 6)
+        Me.divideButton.Name = "divideButton"
+        Me.divideButton.Size = New System.Drawing.Size(75, 75)
+        Me.divideButton.TabIndex = 15
+        Me.divideButton.Text = "/"
+        Me.divideButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.Location = New System.Drawing.Point(12, 6)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(75, 75)
+        Me.clearButton.TabIndex = 14
+        Me.clearButton.Text = "CLEAR"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'equalButton
+        '
+        Me.equalButton.Location = New System.Drawing.Point(255, 246)
+        Me.equalButton.Name = "equalButton"
+        Me.equalButton.Size = New System.Drawing.Size(75, 156)
+        Me.equalButton.TabIndex = 13
+        Me.equalButton.Text = "="
+        Me.equalButton.UseVisualStyleBackColor = True
+        '
+        'addButton
+        '
+        Me.addButton.Location = New System.Drawing.Point(255, 84)
+        Me.addButton.Name = "addButton"
+        Me.addButton.Size = New System.Drawing.Size(75, 156)
+        Me.addButton.TabIndex = 12
+        Me.addButton.Text = "+"
+        Me.addButton.UseVisualStyleBackColor = True
+        '
+        'dotButton
+        '
+        Me.dotButton.Location = New System.Drawing.Point(174, 327)
+        Me.dotButton.Name = "dotButton"
+        Me.dotButton.Size = New System.Drawing.Size(75, 75)
+        Me.dotButton.TabIndex = 11
+        Me.dotButton.Text = "."
+        Me.dotButton.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(12, 327)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(156, 75)
+        Me.Button0.TabIndex = 9
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(174, 246)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 75)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 246)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 75)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 246)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 75)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(174, 165)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 75)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(93, 165)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 75)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 165)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 75)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(174, 84)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 75)
+        Me.Button9.TabIndex = 2
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(93, 84)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 75)
+        Me.Button8.TabIndex = 1
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 84)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 75)
+        Me.Button7.TabIndex = 0
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'enternewButton
+        '
+        Me.enternewButton.AutoSize = True
+        Me.enternewButton.Location = New System.Drawing.Point(12, 408)
+        Me.enternewButton.Name = "enternewButton"
+        Me.enternewButton.Size = New System.Drawing.Size(318, 75)
+        Me.enternewButton.TabIndex = 3
+        Me.enternewButton.Text = "Enter New Numbers"
+        Me.enternewButton.UseVisualStyleBackColor = True
+        '
+        'CalculatorForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 591)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "CalculatorForm"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "My Calculator"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents subtractButton As Button
+    Friend WithEvents multiplyButton As Button
+    Friend WithEvents divideButton As Button
+    Friend WithEvents clearButton As Button
+    Friend WithEvents equalButton As Button
+    Friend WithEvents addButton As Button
+    Friend WithEvents dotButton As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents enternewButton As Button
+End Class
---
+++ Calculator/Calculator/Calculator.vb
@@ -0,0 +1,189 @@
+﻿Public Class CalculatorForm
+
+    Dim num1 As String = "  "
+    Dim num2 As String
+    Dim num3 As String
+    Dim arr() As String = {" ", "*", "/", "+", "-"}
+    Dim ar As Integer = 0
+    Dim ans As String = "="
+
+    Function LabelsettingnunbersFunction(arinput As String, input3 As String) As String
+        If arinput = 0 Then
+            num1 = num1 & input3
+            Label1.Text = num1
+        ElseIf ar = 1 Then
+            num2 = num2 & input3
+            Label2.Text = arr(ar) & num2
+        ElseIf ar = 2 Then
+            num2 = num2 & input3
+            Label2.Text = arr(ar) & num2
+        ElseIf ar = 3 Then
+            num2 = num2 & input3
+            Label2.Text = arr(ar) & num2
+        ElseIf ar = 4 Then
+            num2 = num2 & input3
+            Label2.Text = arr(ar) & num2
+        Else
+            num2 = num2 & "0"
+        End If
+    End Function
+
+    Function IdentifyingFormulaFunction()
+        Label2.Text = arr(ar) & "0"
+    End Function
+
+    Dim num1st
+    Dim num2nd
+    Dim ans1
+    Function MFunction()
+        num1st = Convert.ToDecimal(num1)
+        num2nd = Convert.ToDecimal(num2)
+        ans1 = num1st * num2nd
+        Label3.Text = ans & ans1
+        ButtonDisabling()
+    End Function
+
+    Function DFunction()
+        num1st = Convert.ToDecimal(num1)
+        num2nd = Convert.ToDecimal(num2)
+        ans1 = num1st / num2nd
+        Label3.Text = ans & ans1
+        ButtonDisabling()
+    End Function
+
+    Function AFunction()
+        num1st = Convert.ToDecimal(num1)
+        num2nd = Convert.ToDecimal(num2)
+        ans1 = num1st + num2nd
+        Label3.Text = ans & ans1
+        ButtonDisabling()
+    End Function
+
+    Function SFunction()
+        num1st = Convert.ToDecimal(num1)
+        num2nd = Convert.ToDecimal(num2)
+        ans1 = num1st - num2nd
+        Label3.Text = ans & ans1
+        ButtonDisabling()
+    End Function
+
+    Function ButtonDisabling()
+        Button0.Enabled = False
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+        dotButton.Enabled = False
+        clearButton.Enabled = False
+        multiplyButton.Enabled = False
+        divideButton.Enabled = False
+        subtractButton.Enabled = False
+        addButton.Enabled = False
+        equalButton.Enabled = False
+
+    End Function
+
+    Private Sub offButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        Application.Restart()
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        num3 = "0"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        num3 = "1"
+        LabelsettingnunbersFunction(ar, num3)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        num3 = "2"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        num3 = "3"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        num3 = "4"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        num3 = "5"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        num3 = "6"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        num3 = "7"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        num3 = "8"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        num3 = "9"
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    Private Sub dotButton_Click(sender As Object, e As EventArgs) Handles dotButton.Click
+        num3 = "."
+        LabelsettingnunbersFunction(ar, num3)
+    End Sub
+
+    'sdjkfkewj
+    Private Sub equalButton_Click(sender As Object, e As EventArgs) Handles equalButton.Click
+        If ar = 0 Then
+            Label3.Text = "=0"
+        ElseIf ar = 1 Then
+            MFunction()
+        ElseIf ar = 2 Then
+            DFunction()
+        ElseIf ar = 3 Then
+            AFunction()
+        ElseIf ar = 4 Then
+            SFunction()
+        End If
+
+    End Sub
+
+    Private Sub multiplyButton_Click(sender As Object, e As EventArgs) Handles multiplyButton.Click
+        ar = 1
+        IdentifyingFormulaFunction()
+    End Sub
+    Private Sub divideButton_Click(sender As Object, e As EventArgs) Handles divideButton.Click
+        ar = 2
+        IdentifyingFormulaFunction()
+    End Sub
+
+    Private Sub addButton_Click(sender As Object, e As EventArgs) Handles addButton.Click
+        ar = 3
+        IdentifyingFormulaFunction()
+    End Sub
+
+    Private Sub subtractButton_Click(sender As Object, e As EventArgs) Handles subtractButton.Click
+        ar = 4
+        IdentifyingFormulaFunction()
+    End Sub
+
+    Private Sub enternewButton_Click(sender As Object, e As EventArgs) Handles enternewButton.Click
+        Application.Restart()
+    End Sub
+End Class
